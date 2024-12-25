cefb28242c4c7bd1822fc2496cc2111eab136b8a
itsharshitrwt <harshit.rawat1804@gmail.com>
2024-12-25 16:48:56 +0530

    updated-webiste

--- js/script.js
+++ js/script.js
@@ -74,7 +74,16 @@
       // });
 
     });
+    window.addEventListener('scroll', function() {
+      var navbar = document.getElementById('navbar');
+      if (window.scrollY > 50) { // Adjust the scroll value as needed
+          navbar.classList.add('scrolled');
+      } else {
+          navbar.classList.remove('scrolled');
+      }
+    });
   }
+  
 
 
 
